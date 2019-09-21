8727d470bbb102f46a799fecebe598374e99b788
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-09-21 14:20:09 +0200

    Update AddEntry component

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
 import { getMetricMetaInfo } from '../utils/helpers'
+import UdaciSlider from './UdaciSlider'
+import UdaciSteppers from './UdaciSteppers'
 
 export default class AddEntry extends Component {
   state = {
@@ -38,55 +40,32 @@ export default class AddEntry extends Component {
     }))
   }
   render() {
+    const metaInfo = getMetricMetaInfo()
+
     return (
       <View>
+        {Object.keys(metaInfo).map((key) => {
+          const { getIcon, type, ...rest } = metaInfo[key]
+          const value = this.state[key]
 
-      </View>
-    )
-  }
-}import React, { Component } from 'react'
-import { View } from 'react-native'
-import { getMetricMetaInfo } from '../utils/helpers'
-
-export default class AddEntry extends Component {
-  state = {
-    run: 0,
-    bike: 0,
-    swim: 0,
-    sleep: 0,
-    eat: 0,
-  }
-  increment = (metric) => {
-    const { max, step } = getMetricMetaInfo(metric)
-
-    this.setState((state) => {
-      const count = state[metric] + step
-
-      return {
-        ...state,
-        [metric]: count > max ? max : count,
-      }
-    })
-  }
-  decrement = (metric) => {
-    this.setState((state) => {
-      const count = state[metric] - getMetricMetaInfo(metric).step
-
-      return {
-        ...state,
-        [metric]: count < 0 ? 0 : count,
-      }
-    })
-  }
-  slide = (metric, value) => {
-    this.setState(() => ({
-      [metric]: value
-    }))
-  }
-  render() {
-    return (
-      <View>
-
+          return (
+            <View key={key}>
+              {getIcon()}
+              {type === 'slider'
+                ? <UdaciSlider
+                    value={value}
+                    onChange={(value) => this.slide(key, value)}
+                    {...rest}
+                  />
+                : <UdaciSteppers
+                    value={value}
+                    onIncrement={() => this.increment(key)}
+                    onDecrement={() => this.decrement(key)}
+                    {...rest}
+                  />}
+            </View>
+          )
+        })}
       </View>
     )
   }
