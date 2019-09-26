31d12f361ea854fe80dc3d09ea861ac86c081f60
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-09-26 18:08:21 +0200

    changes for live

--- App.js
+++ App.js
@@ -10,6 +10,9 @@ import { white, purple } from './utils/colors'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import { Constants } from 'expo'
 import EntryDetail from './components/EntryDetail'
+import Live from './components/Live'
+
+
 
 function UdaciStatusBar ({backgroundColor, ...props}) {
   return (
@@ -32,6 +35,13 @@ const Tabs = {
     navigationOptions: {
       tabBarLabel: 'Add Entry',
       tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    },  
+  },
+  Live: {
+    screen: Live,
+    navigationOptions: {
+      tabBarLabel: 'Live',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-speedometer' size={30} color={tintColor} />
     },
   },
 }
