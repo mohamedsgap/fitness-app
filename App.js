6b7219e268a5a9871438e44c70ec846c17d031d9
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-09-26 10:57:15 +0200

    fix an issuer

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { View, Platform, StatusBar } from 'react-native'
+import { View, Platform } from 'react-native'
 import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/History'
-import { TabNavigator } from 'react-navigation'
-import { purple, white } from './utils/colors'
+import { createAppContainer, createBottomTabNavigator, createMaterialTopTabNavigator } from 'react-navigation'
+import { white, purple } from './utils/colors'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import { Constants } from 'expo'
 
@@ -18,7 +18,7 @@ function UdaciStatusBar ({backgroundColor, ...props}) {
   )
 }
 
-const Tabs = TabNavigator({
+const Tabs = {
   History: {
     screen: History,
     navigationOptions: {
@@ -33,10 +33,9 @@ const Tabs = TabNavigator({
       tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
     },
   },
-}, {
-  navigationOptions: {
-    header: null
-  },
+}
+
+const navigationOptions = {
   tabBarOptions: {
     activeTintColor: Platform.OS === 'ios' ? purple : white,
     style: {
@@ -51,15 +50,17 @@ const Tabs = TabNavigator({
       shadowOpacity: 1
     }
   }
-})
+}
+
+const TabNav = createAppContainer(Platform.OS === 'ios' ? createBottomTabNavigator(Tabs, navigationOptions) : createMaterialTopTabNavigator(Tabs, navigationOptions))
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
-          <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
-          <Tabs />
+        <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
+          <TabNav />
         </View>
       </Provider>
     )
