ae3f28851ad729a3e6c4b1c3eea82a4a14ab89bc
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-09-26 18:44:42 +0200

    Updates for animated

--- components/Live.js
+++ components/Live.js
@@ -10,7 +10,8 @@ export default class Live extends Component {
     state = {
       coords: null,
       status: null,
-      direction: ''
+      direction: '',
+      bounceValue: new Animated.Value(1),
     }
     componentDidMount () {
       Permissions.getAsync(Permissions.LOCATION)
@@ -47,6 +48,13 @@ export default class Live extends Component {
         const newDirection = calculateDirection(coords.heading)
         const { direction, bounceValue } = this.state
   
+        if (newDirection !== direction) {
+          Animated.sequence([
+            Animated.timing(bounceValue, { duration: 200, toValue: 1.04}),
+            Animated.spring(bounceValue, { toValue: 1, friction: 4})
+          ]).start()
+        }
+  
         this.setState(() => ({
           coords,
           status: 'granted',
@@ -55,7 +63,7 @@ export default class Live extends Component {
       })
     }
     render() {
-      const { status, coords, direction } = this.state
+      const { status, coords, direction, bounceValue } = this.state
   
       if (status === null) {
         return <ActivityIndicator style={{marginTop: 30}}/>
@@ -92,9 +100,10 @@ export default class Live extends Component {
         <View style={styles.container}>
           <View style={styles.directionContainer}>
             <Text style={styles.header}>You're heading</Text>
-            <Text style={styles.direction}>
-              {direction}
-            </Text>
+            <Animated.Text
+              style={[styles.direction, {transform: [{scale: bounceValue}]}]}>
+                {direction}
+            </Animated.Text>
           </View>
           <View style={styles.metricContainer}>
             <View style={styles.metric}>
