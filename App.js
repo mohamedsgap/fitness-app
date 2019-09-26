b0f3375f320bc8b69e4fb639e992c6a998e84d55
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-09-26 12:55:19 +0200

    create a StackNavigator

--- App.js
+++ App.js
@@ -5,10 +5,11 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/History'
-import { createAppContainer, createBottomTabNavigator, createMaterialTopTabNavigator } from 'react-navigation'
+import { createAppContainer, createBottomTabNavigator, createMaterialTopTabNavigator, StackNavigator } from 'react-navigation'
 import { white, purple } from './utils/colors'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import { Constants } from 'expo'
+import EntryDetail from './components/EntryDetail'
 
 function UdaciStatusBar ({backgroundColor, ...props}) {
   return (
@@ -54,13 +55,30 @@ const navigationOptions = {
 
 const TabNav = createAppContainer(Platform.OS === 'ios' ? createBottomTabNavigator(Tabs, navigationOptions) : createMaterialTopTabNavigator(Tabs, navigationOptions))
 
+
+const MainNavigator = StackNavigator({
+  Home: {
+    screen: Tabs,
+  },
+  EntryDetail: {
+    screen: EntryDetail,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple,
+      }
+    }
+  }
+})
+
+
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
         <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
-          <TabNav />
+          <StackNavigator/>
         </View>
       </Provider>
     )
